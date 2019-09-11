fae9d683259559ccd22564d97e748da6ab4be744
Riley <rileydbecker@gmail.com>
2019-09-10 23:06:05 -0400

    Also delete the filters from the main file

--- modify.py
+++ modify.py
@@ -258,21 +258,6 @@ def unblockCard(args): ## -z
 def test(args):
   nums = [1,2,3,4,5,656,76,7,8,899,8,7,6]
 
-def filter1(cardInfos):
-  newCards = {}
-  for card in cardInfos.keys():
-    if not cardInfos[card].status.complete:
-      newCards[card] = cardInfos[card]
-  return newCards
-
-def filter2(cardInfos,config):
-  newCards = {}
-  for card in cardInfos.keys():
-    info = cardInfos[card]
-    if info.status.complete and info.card >= config[0] and info.card < config[1]:
-      newCards[card] = cardInfos[card]
-  return newCards
-
 def isValid(args, minVal):
   return len(args) >= minVal
 
